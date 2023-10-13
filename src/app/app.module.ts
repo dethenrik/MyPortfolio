135015fa06c51ab7970959d8c98ffc5343e764d8
dethenrik <almand1999@hotmail.com>
2023-10-13 12:26:03 +0200

    A LOT OG FILES HAVE BEEN ADDED AND CHANGED

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfilComponent } from './profil/profil.component';
+import { OpgaverComponent } from './opgaver/opgaver.component';
+import { CVComponent } from './cv/cv.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { BeviserComponent } from './beviser/beviser.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfilComponent,
+    OpgaverComponent,
+    CVComponent,
+    TimelineComponent,
+    BeviserComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,
